238136d9ea5d38f8919e824ec5459724d4145245
DaffyLM <dagfinn.markussen@gmail.com>
2023-09-08 11:51:09 +0200

    Her har jeg slettet testen

--- Script/GitTutorial.R
+++ Script/GitTutorial.R
@@ -6,6 +6,3 @@ use_git_config(user.name = "DaffyLM", user.email = "dagfinn.markussen@gmail.com"
 install.packages("devtools")
 devtools::install_github("jromanowska/BasicGitTutorial")
 
-a<-1
-b<-2
-a+b
