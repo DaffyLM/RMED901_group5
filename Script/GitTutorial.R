dd9ca207038e8b34c5a07e4ee03065d21aa9b1d3
DaffyLM <dagfinn.markussen@gmail.com>
2023-09-08 11:46:48 +0200

    Dette var en test av versjonskontroll

--- Script/GitTutorial.R
+++ Script/GitTutorial.R
@@ -5,3 +5,7 @@ use_git_config(user.name = "DaffyLM", user.email = "dagfinn.markussen@gmail.com"
 
 install.packages("devtools")
 devtools::install_github("jromanowska/BasicGitTutorial")
+
+a<-1
+b<-2
+a+b
