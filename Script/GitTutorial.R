078bf7753a7fc681317c24d96a3d0fcac3e5e0dc
DaffyLM <dagfinn.markussen@gmail.com>
2023-09-08 11:42:10 +0200

    This is the start of the project

---
+++ Script/GitTutorial.R
@@ -0,0 +1,7 @@
+
+install.packages("usethis")
+library(usethis)
+use_git_config(user.name = "DaffyLM", user.email = "dagfinn.markussen@gmail.com")
+
+install.packages("devtools")
+devtools::install_github("jromanowska/BasicGitTutorial")
