0a5186ff260558b6ff422963d5c2a3778a40aad8
DaffyLM <dagfinn.markussen@gmail.com>
2023-09-12 14:18:38 +0200

    Fjernet et duplikat

--- Script/Script_group5.R
+++ Script/Script_group5.R
@@ -1,1 +1,14 @@
 library(tidyverse)
+install.packages("here")
+library(here)
+here()
+
+OurData <- read_tsv(here("DATA", "exam_data.txt"))
+OurData
+glimpse(OurData)
+head(OurData)
+tail(OurData)
+view(OurData)
+
+OurData <- OurData %>%
+  distinct()
