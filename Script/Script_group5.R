7eae047014577355e12bd6a61adfb23508572d70
Mathilde Hemminghyth <mathildesh@hotmail.com>
2023-09-12 14:21:18 +0200

    Renamed variables

--- Script/Script_group5.R
+++ Script/Script_group5.R
@@ -1,1 +1,15 @@
 library(tidyverse)
+library(here)
+here()
+read_tsv(here("Data","exam_data.txt"))
+OurData <- read_tsv(here("Data", "exam_data.txt"))
+head(OurData)
+view(OurData)
+glimpse(OurData)
+
+OurData <-
+  OurData %>%
+  rename(BMI = `BMI kg/m2`,
+         Preg.ended_bf37 = `Preg.ended<37wk`)
+
+glimpse(OurData)
