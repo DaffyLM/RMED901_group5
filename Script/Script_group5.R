384b1ac9f06eae676313fc482819b98c99697d02
ingvildforthun <ingvildhalsor@gmail.com>
2023-09-12 14:39:52 +0200

    Group project, task 3.4

--- Script/Script_group5.R
+++ Script/Script_group5.R
@@ -1,1 +1,18 @@
 library(tidyverse)
+library(here)
+read_tsv(here("Data", "exam_data.txt"))
+# Read the file
+
+OurData <- read_tsv(here("Data", "exam_data.txt"))
+
+OurData %>% 
+  glimpse()
+# Local_Topical.Anest is also combined, with information on both Local and Topical Anesthetic 
+
+OurData <- OurData %>%
+  separate (col = Local_Topical.Anest, into = c("LocalAnesthetic", "TopicalAnesthetic"),
+            sep = "_")
+# Changed column Local_Topical.Anest with combined variables to two different variables. 
+
+OurData %>% 
+  glimpse()
