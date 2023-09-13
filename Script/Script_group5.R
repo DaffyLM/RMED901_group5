15084fc0dacc6e138c5a3519e996471c5461bec9
DaffyLM <dagfinn.markussen@gmail.com>
2023-09-13 13:10:04 +0200

    Piped the commands from yeaterday, converted coloumn c to eiter c or t. Renamed coloumn C to Group

--- Script/Script_group5.R
+++ Script/Script_group5.R
@@ -11,10 +11,20 @@ glimpse(OurData)
 View(OurData)        
 skimr::skim(OurData) 
 
-
+#Piped the commands
 OurData <- OurData %>%
   rename(BMI = `BMI kg/m2`,
          Preg.ended_bf37 = `Preg.ended<37wk`) %>%
   naniar::gg_miss_var() %>%
   separate(col = Local_Topical.Anest, into = c("LocalAnesthetic", "TopicalAnesthetic"), sep = "_") %>%
   distinct()
+
+#Converted coloumn C to either C or T
+OurData <- OurData %>%
+  mutate(C = case_when(C == "TRUE" ~ "C",
+                       TRUE ~ "T"))
+
+#Rename coloumn C to Group
+OurData <- OurData %>% 
+  rename(Group = C)
+View(OurData)
