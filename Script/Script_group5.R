296b5ece1cdf9369dfb467792510da4c4a21dd45
ingvildforthun <ingvildhalsor@gmail.com>
2023-09-13 13:27:57 +0200

    Read and join additional dataset and changed names of the new values

--- Script/Script_group5.R
+++ Script/Script_group5.R
@@ -17,22 +17,6 @@ OurData <- OurData %>%
 OurData %>% 
   glimpse()
 
-install.packages("here")
-library(here)
-here()
-
-OurData <- read_tsv(here("DATA", "exam_data.txt"))
-OurData
-glimpse(OurData)
-head(OurData)
-tail(OurData)
-view(OurData)
-
-#<<<<<<< DLM
-#Remove duplicates
-OurData <- OurData %>%
-  distinct()
-
 #Endret fra 835 til 834 rader
 
 #Variable types
@@ -44,6 +28,21 @@ OurData <- OurData %>%
 #EDC.necessary should be logical not string
 #Same for ALL binary/factor/logical variables
 
-OurData <- OurData %>%
-  distinct()
+#Read the join data
+OurData_join <- read_tsv(here("Data", "exam_data_join.txt"))
 
+#Join the new dataset with the "old"
+OurData <- OurData %>%
+  full_join(OurData_join, by = join_by(PID))
+
+#Rename the new variables O61 and O81
+OurData <- 
+  OurData %>%
+  rename(IL6_baseline = O61)
+
+OurData <-
+  OurData %>%
+  rename(IL8_baseline = O81)
+
+OurData %>% 
+  glimpse()
